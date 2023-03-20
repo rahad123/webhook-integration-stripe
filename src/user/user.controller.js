1c622b45435fdf65d110e62d2f198dd51a2e98fa
rahad123 <rahaddiu@gmail.com>
2023-03-20 16:00:28 +0600

    fix: "unused code has cleaned"

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,12 +1,10 @@
 const { createUser } = require("./user.service");
 
 module.exports = {
-  // Get the home page:
   createUser: async (req, res) => {
     const User = req.body;
-    console.log("User", User);
     const user = await createUser(User);
-    res.send("success", user);
+    res.send(user);
   },
   getUsers: async (_, args, ctx) => {
     try {
