7bfc87040b32b6adf69fe83f24db230bed84e4b7
rahad123 <rahaddiu@gmail.com>
2023-03-20 15:59:50 +0600

    fix: "DB has modified"

--- src/db/db.js
+++ src/db/db.js
@@ -8,12 +8,6 @@ const { db, app } = require("../config/config");
 const dbURL = `${db.uri}-${app?.environment}`;
 if (!dbURL) console.error("Mongo URL not set in env file or config.js");
 
-mongoose.connect(dbURL, (error) => {
-  if (error) {
-    console.error(`failed to connect using mongoose. ${error}`);
-  } else {
-    console.info(`Connected to DB server. ${dbURL}`);
-  }
-});
+mongoose.connect(dbURL);
 
 module.exports = mongoose;
