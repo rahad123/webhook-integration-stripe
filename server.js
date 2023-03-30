2db2329f5dac3cfec4206a6dfd295150ceaf17d2
rahad123 <rahaddiu@gmail.com>
2023-03-31 00:47:29 +0600

    fix: server file refactored

--- server.js
+++ server.js
@@ -4,19 +4,9 @@ const bodyParser = require('body-parser');
 const express = require("express");
 const apiRouter = require("./src/routes/api");
 
-// const { connectedToStripe } = require("./src/stripe/stripe.controller");
-
-// const { addMember } = require("./src/members/members.controller");
-
 const app = express();
 app.use(bodyParser.json());
 const port = 3000;
-
-// Stripe
-// app.get("/connected", connectedToStripe);
-
-// // members
-// app.post("member", addMember);
 
 app.use("/api", apiRouter);
 
